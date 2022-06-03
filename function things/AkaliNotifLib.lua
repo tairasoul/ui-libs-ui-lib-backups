980699692278f48fbb057fa761e83750475406b8
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-03 12:55:37 +0200

    Create AkaliNotifLib.lua

---
+++ function things/AkaliNotifLib.lua
@@ -0,0 +1,9 @@
+function SendAkaliNotification(titlestring, infostring, timetowait)
+    local AkaliNotif = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/batusz/uilibrarys/main/AkaliNotifLib"))();
+    local Notify = AkaliNotif.Notify;
+    Notify({
+    Description = ""..infostring;
+    Title = ""..titlestring;
+    Duration = timetowait;
+    });
+end
