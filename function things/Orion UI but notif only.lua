1afcdfb6f9a48b370e91f1ea632d943266d85e8d
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-21 15:33:05 +0200

    Update Orion UI but notif only.lua

--- function things/Orion UI but notif only.lua
+++ function things/Orion UI but notif only.lua
@@ -1,10 +1,13 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 function OrionNotif(title, content, image, duration)
-if typeof(image) == "number" then image = duration end
+if typeof(image) == "number" then 
+	duration = image 
+	image = nil 
+end
 OrionLib:MakeNotification({
-	Name = tostring(title) or "This is a standard title.",
-	Content = tostring(content) or "This is a standard content line.",
+	Name = title or "This is a standard title.",
+	Content = content or "This is a standard content line.",
 	Image = image or "rbxassetid://4483345998",
 	Time = duration or 5
 })
