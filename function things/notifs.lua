4d1427f2817d1a86068a85cebe9cc429d135ff88
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-22 14:16:30 +0200

    Update notifs.lua

--- function things/notifs.lua
+++ function things/notifs.lua
@@ -3,23 +3,24 @@ local Notify = AkaliNotif.Notify;
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 function NotifLib:Init()
-  function NotifLib:Akali(titlestring, infostring, timetowait)
-      Notify({
-      Description = infostring;
-      Title = titlestring;
-      Duration = timetowait;
-      });
-  end
-  function NotifLib:Orion(title, content, image, duration)
-  if typeof(image) == "number" then 
-    duration = image 
-    image = nil 
-  end
-  OrionLib:MakeNotification({
-    Name = title or "This is a standard title.",
-    Content = content or "This is a standard content line.",
-    Image = image or "rbxassetid://4483345998",
-    Time = duration or 5
-  })
-  end
-return NotifLib
+    function NotifLib:Akali(titlestring, infostring, timetowait)
+        Notify({
+        Description = infostring;
+        Title = titlestring;
+        Duration = timetowait;
+        });
+    end
+    function NotifLib:Orion(title, content, image, duration)
+    if typeof(image) == "number" then 
+      duration = image 
+      image = nil 
+    end
+    OrionLib:MakeNotification({
+      Name = title or "This is a standard title.",
+      Content = content or "This is a standard content line.",
+      Image = image or "rbxassetid://4483345998",
+      Time = duration or 5
+    })
+    end
+  return NotifLib
+end
